ff4c80a4b862c9294ed7a13f4be9211469eff5ed
skye7012 <skye7012@gmail.com>
2021-07-09 10:54:35 +0300

    minus comm

--- TaskTrackerWebApi/Models/TaskTrackerContext.cs
+++ TaskTrackerWebApi/Models/TaskTrackerContext.cs
@@ -25,7 +25,6 @@ namespace TaskTrackerWebApi.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //optionsBuilder.UseSqlServer("Server=desktop-bdt1agl;Database=TaskTracker;Trusted_Connection=True;");
                 optionsBuilder.UseSqlServer(File.ReadAllText("connectionString.txt"));
             }
         }
